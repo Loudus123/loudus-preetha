9707f19cdef5b858a7cfa1a17468a2a7925d471c
Loudus123 <loudusraj@gmail.com>
2016-07-10 11:30:01 +0530

    Update and rename README.md to arrayasending.c
    finish

---
+++ arrayasending.c
@@ -0,0 +1,28 @@
+# loudus-preetha
+#include<stdio.h>
+void main()
+{
+int a[50],x,i,j,temp=0;
+clrscr();
+printf("Enter the size of array\n");
+scanf("%d",&x);
+printf("Enter the element one by one:\n");
+for(i=0;i<x;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<x;i++)
+for(j=i+1;j<x;j++
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+printf("after arrange\n");
+for(i=0;i<x;i++)
+printf("%d\t",a[i]);
+getch();
+}
